ebde4a42037abfbf97faaa7239e63932ed375b63
Stefan Frömken <froemken@gmail.com>
2023-08-25 16:28:20 +0200

    Reformat structure of ext_tables.sql

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,7 +1,8 @@
 #
 # Table structure for table 'sys_file'
 #
-CREATE TABLE sys_file (
-	cruser_id int(11) unsigned DEFAULT '0',
+CREATE TABLE sys_file
+(
+	cruser_id    int(11) unsigned DEFAULT '0',
 	fe_cruser_id int(11) unsigned DEFAULT '0'
 );
