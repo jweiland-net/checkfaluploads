dce25661ff8b1ee814a9428a0d3727519acc4b4d
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-01-09 19:08:21 +0100

    [TASK] Deprecated FALUploadService as it is used in several updated extensions keep it now

--- Classes/Service/FalUploadService.php
+++ Classes/Service/FalUploadService.php
@@ -20,9 +20,20 @@ use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
  * of the image to the owner of the website.
  * Instead of implementing this code in many of our JW extensions we simply check, if checkfaluploads is loaded
  * and call this Service to return an Extbase Error, if checkbox was not marked.
+ *
+ * @deprecated
+ * FalUploadService is deprecated. File rights are now handled via PSR-15 middleware in EXT:checkfaluploads.
  */
 class FalUploadService
 {
+    public function __construct()
+    {
+        trigger_error(
+            'FalUploadService is deprecated. File rights are now handled via PSR-15 middleware in EXT:checkfaluploads.',
+            E_USER_DEPRECATED
+        );
+    }
+
     public function checkFile(
         UploadedFile $uploadedFile,
         ?array $rightsConfiguration,
@@ -30,55 +41,7 @@ class FalUploadService
         string $langKey = 'error.uploadFile.missingRights',
         string $extensionName = 'checkfaluploads',
     ): ?Error {
-        // Check if the file has an upload error
-        if ($uploadedFile->getError() !== UPLOAD_ERR_OK) {
-            return new Error(
-                LocalizationUtility::translate(
-                    'error.uploadFile.invalidFile',
-                    $extensionName,
-                ) . ': ' . $this->getUploadErrorMessage($uploadedFile->getError()),
-                1604050226,
-            );
-        }
-
-        // Check if the uploaded file has content (i.e., is not empty)
-        if ($uploadedFile->getSize() === 0) {
-            return new Error(
-                LocalizationUtility::translate('error.uploadFile.emptyFile', $extensionName),
-                1604050227,
-            );
-        }
-
-        // Check the rightsConfigurations set for the field or not
-        if (!isset($rightsConfiguration[$fieldName]) || $rightsConfiguration[$fieldName] === '' || $rightsConfiguration[$fieldName] === 0) {
-            return new Error(
-                LocalizationUtility::translate($langKey, $extensionName),
-                1604050225,
-            );
-        }
 
         return null;
-    }
-
-    private function getUploadErrorMessage(int $errorCode): string
-    {
-        switch ($errorCode) {
-            case UPLOAD_ERR_INI_SIZE:
-                return 'The uploaded file exceeds the upload_max_filesize directive in php.ini.';
-            case UPLOAD_ERR_FORM_SIZE:
-                return 'The uploaded file exceeds the MAX_FILE_SIZE directive specified in the HTML form.';
-            case UPLOAD_ERR_PARTIAL:
-                return 'The uploaded file was only partially uploaded.';
-            case UPLOAD_ERR_NO_FILE:
-                return 'No file was uploaded.';
-            case UPLOAD_ERR_NO_TMP_DIR:
-                return 'Missing a temporary folder.';
-            case UPLOAD_ERR_CANT_WRITE:
-                return 'Failed to write file to disk.';
-            case UPLOAD_ERR_EXTENSION:
-                return 'A PHP extension stopped the file upload.';
-            default:
-                return 'Unknown upload error.';
-        }
     }
 }
