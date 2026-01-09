f207a7c1e719a3fed3215c00eba6e13a07de527c
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-01-09 23:52:02 +0100

    [BUGFIX] Small CGL level fix

--- Classes/Middleware/CheckFileUploadsMiddleware.php
+++ Classes/Middleware/CheckFileUploadsMiddleware.php
@@ -3,7 +3,8 @@
 declare(strict_types=1);
 
 /*
- * This file is part of the package jweiland/EXT_KEY.
+ * This file is part of the package jweiland/checkfaluploads.
+ *
  * For the full copyright and license information, please read the
  * LICENSE file that was distributed with this source code.
  */
