1396748824c08254e77bb2affa53c9922e8bce9b
Hoja Mustaffa Abdul Latheef <hlatheef@jweiland.net>
2026-02-09 19:32:35 +0100

    [TASK] Cleanup ext_localconf.php by removing redundant wrapper
    Removes the unnecessary call_user_func wrapper while maintaining
    the global configuration overrides. This cleans up the file
    structure without affecting the functional logic of the XCLASS
    registrations and form hooks.

--- ext_localconf.php
+++ ext_localconf.php
@@ -18,15 +18,13 @@ use JWeiland\Checkfaluploads\RecordList\View\FolderUtilityRenderer as Checkfalup
 use TYPO3\CMS\Backend\View\FolderUtilityRenderer;
 use TYPO3\CMS\Filelist\Controller\FileListController;
 
-call_user_func(static function (): void {
-    // Disable CSS class "t3js-drag-uploader-trigger" to prevent loading DragUploader modal.
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][FileListController::class]['className'] = CheckfaluploadsFileListController::class;
+// Disable CSS class "t3js-drag-uploader-trigger" to prevent loading DragUploader modal.
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][FileListController::class]['className'] = CheckfaluploadsFileListController::class;
 
-    // Add userHasRights checkbox to FileBrowser PopUp
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][FolderUtilityRenderer::class]['className'] = CheckfaluploadsFolderUtilityRenderer::class;
+// Add userHasRights checkbox to FileBrowser PopUp
+$GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects'][FolderUtilityRenderer::class]['className'] = CheckfaluploadsFolderUtilityRenderer::class;
 
-    // Update (replace placeholders) label and description of Checkboxes
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterBuildingFinished'][1661929818] = ReplacePlaceholderHook::class;
-    // Register dynamic validation which depends on other submitted form element values (image -> upload-rights)
-    $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterSubmit'][1661258175] = DynamicUploadValidatorHook::class;
-});
+// Update (replace placeholders) label and description of Checkboxes
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterBuildingFinished'][1661929818] = ReplacePlaceholderHook::class;
+// Register dynamic validation which depends on other submitted form element values (image -> upload-rights)
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['ext/form']['afterSubmit'][1661258175] = DynamicUploadValidatorHook::class;
