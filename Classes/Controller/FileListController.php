ab339ae9196f8310555fc98b51b07f4a59bcde04
Stefan Froemken <sfroemken@jweiland.net>
2026-09-02 14:20:27 +0200

    Revert 436b19cc from Hoja. Never try upgrading XCLASSed code from Core. Backport from Core instead

--- Classes/Controller/FileListController.php
+++ Classes/Controller/FileListController.php
@@ -18,6 +18,7 @@ use TYPO3\CMS\Backend\Template\Components\Buttons\DropDown\DropDownDivider;
 use TYPO3\CMS\Backend\Template\Components\Buttons\DropDown\DropDownItem;
 use TYPO3\CMS\Backend\Template\Components\Buttons\DropDown\DropDownRadio;
 use TYPO3\CMS\Backend\Template\Components\Buttons\DropDown\DropDownToggle;
+use TYPO3\CMS\Core\Imaging\Icon;
 use TYPO3\CMS\Core\Imaging\IconSize;
 use TYPO3\CMS\Core\Resource\Folder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
@@ -29,8 +30,6 @@ use TYPO3\CMS\Filelist\Type\ViewMode;
  */
 class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListController
 {
-    protected const ICON_SIZE = IconSize::SMALL;
-
     /**
      * Create the panel of buttons for submitting the form or otherwise perform operations.
      */
@@ -40,41 +39,13 @@ class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListControll
         $buttonBar = $this->view->getDocHeaderComponent()->getButtonBar();
 
         // Refresh
-        $this->addRefreshButton($request, $buttonBar, $lang);
-
-        // ViewMode
-        $this->addViewModeButtons($buttonBar, $lang);
-
-        // Level up
-        $this->addLevelUpButton($buttonBar, $lang);
-
-        // Shortcut
-        $this->addShortCutButton($buttonBar, $lang);
-
-        // Upload button (only if upload to this directory is allowed)
-        $this->addUploadButton($buttonBar, $lang);
-
-        // New folder button
-        $this->addNewFolderButton($buttonBar, $lang);
-
-        // New file button
-        $this->addNewFileButton($buttonBar, $lang);
-
-        // Add paste button if clipboard is initialized
-        $this->addPasteButton($buttonBar, $lang);
-    }
-
-    private function addRefreshButton(ServerRequestInterface $request, ButtonBar $buttonBar, $lang): void
-    {
         $refreshButton = $buttonBar->makeLinkButton()
             ->setHref($request->getAttribute('normalizedParams')->getRequestUri())
             ->setTitle($lang->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.reload'))
-            ->setIcon($this->iconFactory->getIcon('actions-refresh', self::ICON_SIZE));
+            ->setIcon($this->iconFactory->getIcon('actions-refresh', IconSize::SMALL));
         $buttonBar->addButton($refreshButton, ButtonBar::BUTTON_POSITION_RIGHT);
-    }
 
-    private function addViewModeButtons(ButtonBar $buttonBar, $lang): void
-    {
+        // ViewMode
         $viewModeItems = [];
         $viewModeItems[] = GeneralUtility::makeInstance(DropDownRadio::class)
             ->setActive($this->moduleData->get('viewMode') === ViewMode::TILES->value)
@@ -109,7 +80,7 @@ class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListControll
                 ->setAttributes([
                     'data-url' => $this->uriBuilder->buildUriFromRoute(
                         'ajax_show_columns_selector',
-                        ['id' => $this->id, 'table' => '_FILE'],
+                        ['id' => $this->id, 'table' => '_FILE']
                     ),
                     'data-target' => $this->filelist->createModuleUri(),
                     'data-title' => sprintf(
@@ -131,109 +102,48 @@ class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListControll
         }
 
         $buttonBar->addButton($viewModeButton, ButtonBar::BUTTON_POSITION_RIGHT, 2);
-    }
 
-    private function getViewModeItems($lang): array
-    {
-        $viewModeItems = [
-            $this->createDropDownRadio(ViewMode::TILES, $lang),
-            $this->createDropDownRadio(ViewMode::LIST, $lang),
-            GeneralUtility::makeInstance(DropDownDivider::class),
-        ];
-
-        // Add thumbnail toggle if enabled
-        if ($this->isThumbnailDisplayEnabled()) {
-            $viewModeItems[] = $this->createDropDownToggle('actions-image', $lang, 'showThumbnails', 'displayThumbs');
-        }
-
-        $viewModeItems[] = $this->createDropDownToggle('actions-clipboard', $lang, 'showClipboard', 'clipBoard');
-
-        // Column selector in list view
-        if ($this->isColumnSelectorEnabled()) {
-            $viewModeItems[] = $this->createColumnSelectorItem($lang);
-        }
-
-        return $viewModeItems;
-    }
-
-    private function createDropDownRadio(string $viewMode, $lang): DropDownRadio
-    {
-        return GeneralUtility::makeInstance(DropDownRadio::class)
-            ->setActive($this->moduleData->get('viewMode') === $viewMode)
-            ->setHref($this->filelist->createModuleUri(['viewMode' => $viewMode]))
-            ->setLabel($lang->sL("LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.view.{$viewMode}"))
-            ->setIcon($this->iconFactory->getIcon("actions-viewmode-{$viewMode}"));
-    }
-
-    private function isThumbnailDisplayEnabled(): bool
-    {
-        return $GLOBALS['TYPO3_CONF_VARS']['GFX']['thumbnails'] &&
-            ($this->getBackendUser()->getTSConfig()['options.']['file_list.']['enableDisplayThumbnails'] ?? '') === 'selectable';
-    }
-
-    private function isColumnSelectorEnabled(): bool
-    {
-        return ($this->getBackendUser()->getTSConfig()['options.']['file_list.']['displayColumnSelector'] ?? true)
-            && $this->moduleData->get('viewMode') === ViewMode::LIST->value;
-    }
-
-    private function addLevelUpButton(ButtonBar $buttonBar, $lang): void
-    {
+        // Level up
         try {
-            $parentFolder = $this->getParentFolder();
-            if ($parentFolder) {
+            $currentStorage = $this->folderObject->getStorage();
+            $parentFolder = $this->folderObject->getParentFolder();
+            if ($currentStorage->isWithinFileMountBoundaries($parentFolder)
+                && $parentFolder->getIdentifier() !== $this->folderObject->getIdentifier()
+                && $parentFolder instanceof Folder
+            ) {
                 $levelUpButton = $buttonBar->makeLinkButton()
                     ->setDataAttributes([
-                        'tree-update-request' => htmlspecialchars(
-                            'folder' . GeneralUtility::md5int($parentFolder->getCombinedIdentifier()),
-                        ),
+                        'tree-update-request' => htmlspecialchars('folder' . GeneralUtility::md5int($parentFolder->getCombinedIdentifier())),
                     ])
                     ->setHref(
                         (string)$this->uriBuilder->buildUriFromRoute(
                             'media_management',
-                            ['id' => $parentFolder->getCombinedIdentifier()],
-                        ),
+                            ['id' => $parentFolder->getCombinedIdentifier()]
+                        )
                     )
                     ->setShowLabelText(true)
-                    ->setTitle(
-                        $lang->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.upOneLevel'),
-                    )
-                    ->setIcon($this->iconFactory->getIcon('actions-view-go-up', self::ICON_SIZE));
+                    ->setTitle($lang->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:labels.upOneLevel'))
+                    ->setIcon($this->iconFactory->getIcon('actions-view-go-up', IconSize::SMALL));
                 $buttonBar->addButton($levelUpButton, ButtonBar::BUTTON_POSITION_LEFT, 1);
             }
         } catch (\Exception $exception) {
         }
-    }
 
-    private function getParentFolder(): ?Folder
-    {
-        $currentStorage = $this->folderObject->getStorage();
-        $parentFolder = $this->folderObject->getParentFolder();
-        return $currentStorage->isWithinFileMountBoundaries($parentFolder) &&
-        $parentFolder->getIdentifier() !== $this->folderObject->getIdentifier() &&
-        $parentFolder instanceof Folder
-            ? $parentFolder
-            : null;
-    }
-
-    private function addShortCutButton(ButtonBar $buttonBar, $lang): void
-    {
+        // Shortcut
         $shortCutButton = $buttonBar->makeShortcutButton()
             ->setRouteIdentifier('media_management')
             ->setDisplayName(sprintf(
                 '%s: %s',
                 $lang->sL('LLL:EXT:filelist/Resources/Private/Language/locallang_mod_file_list.xlf:mlang_tabs_tab'),
-                $this->folderObject->getName() ?: $this->folderObject->getIdentifier(),
+                $this->folderObject->getName() ?: $this->folderObject->getIdentifier()
             ))
             ->setArguments(array_filter([
                 'id' => $this->id,
                 'searchTerm' => $this->searchTerm,
             ]));
         $buttonBar->addButton($shortCutButton, ButtonBar::BUTTON_POSITION_RIGHT);
-    }
 
-    private function addUploadButton(ButtonBar $buttonBar, $lang): void
-    {
+        // Upload button (only if upload to this directory is allowed)
         if ($this->folderObject
             && $this->folderObject->checkActionPermission('write')
             && $this->folderObject->getStorage()->checkUserActionPermission('add', 'File')
@@ -243,16 +153,12 @@ class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListControll
                 //->setClasses('t3js-drag-uploader-trigger')
                 ->setShowLabelText(true)
                 ->setTitle($lang->sL('LLL:EXT:core/Resources/Private/Language/locallang_core.xlf:cm.upload'))
-                ->setIcon($this->iconFactory->getIcon('actions-edit-upload', self::ICON_SIZE));
+                ->setIcon($this->iconFactory->getIcon('actions-edit-upload', IconSize::SMALL));
             $buttonBar->addButton($uploadButton, ButtonBar::BUTTON_POSITION_LEFT, 2);
         }
-    }
 
-    private function addNewFolderButton(ButtonBar $buttonBar, \TYPO3\CMS\Core\Localization\LanguageService $lang): void
-    {
-        if ($this->folderObject && $this->folderObject->checkActionPermission(
-            'write',
-        ) && $this->folderObject->checkActionPermission('add')) {
+        // New folder button
+        if ($this->folderObject && $this->folderObject->checkActionPermission('write') && $this->folderObject->checkActionPermission('add')) {
             $newButton = $buttonBar->makeLinkButton()
                 ->setClasses('t3js-element-browser')
                 ->setHref((string)$this->uriBuilder->buildUriFromRoute('wizard_element_browser'))
@@ -262,35 +168,29 @@ class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListControll
                 ])
                 ->setShowLabelText(true)
                 ->setTitle($lang->sL('LLL:EXT:filelist/Resources/Private/Language/locallang.xlf:actions.create_folder'))
-                ->setIcon($this->iconFactory->getIcon('actions-folder-add', self::ICON_SIZE));
+                ->setIcon($this->iconFactory->getIcon('actions-folder-add', IconSize::SMALL));
             $buttonBar->addButton($newButton, ButtonBar::BUTTON_POSITION_LEFT, 3);
         }
-    }
 
-    private function addNewFileButton(ButtonBar $buttonBar, \TYPO3\CMS\Core\Localization\LanguageService $lang): void
-    {
+        // New file button
         if ($this->folderObject && $this->folderObject->checkActionPermission('write')
             && $this->folderObject->getStorage()->checkUserActionPermission('add', 'File')
         ) {
             $newButton = $buttonBar->makeLinkButton()
-                ->setHref(
-                    (string)$this->uriBuilder->buildUriFromRoute(
-                        'file_create',
-                        [
-                            'target' => $this->folderObject->getCombinedIdentifier(),
-                            'returnUrl' => $this->filelist->createModuleUri(),
-                        ],
-                    ),
-                )
+                ->setHref((string)$this->uriBuilder->buildUriFromRoute(
+                    'file_create',
+                    [
+                        'target' => $this->folderObject->getCombinedIdentifier(),
+                        'returnUrl' => $this->filelist->createModuleUri(),
+                    ]
+                ))
                 ->setShowLabelText(true)
                 ->setTitle($lang->sL('LLL:EXT:filelist/Resources/Private/Language/locallang.xlf:actions.create_file'))
-                ->setIcon($this->iconFactory->getIcon('actions-file-add', self::ICON_SIZE));
+                ->setIcon($this->iconFactory->getIcon('actions-file-add', IconSize::SMALL));
             $buttonBar->addButton($newButton, ButtonBar::BUTTON_POSITION_LEFT, 4);
         }
-    }
 
-    private function addPasteButton(ButtonBar $buttonBar, \TYPO3\CMS\Core\Localization\LanguageService $lang): void
-    {
+        // Add paste button if clipboard is initialized
         if ($this->filelist->clipObj instanceof Clipboard && $this->folderObject->checkActionPermission('write')) {
             $elFromTable = $this->filelist->clipObj->elFromTable('_FILE');
             if (!empty($elFromTable)) {
@@ -299,9 +199,9 @@ class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListControll
                 foreach ($elFromTable as $key => $element) {
                     $clipBoardElement = $this->resourceFactory->retrieveFileOrFolderObject($element);
                     if ($clipBoardElement instanceof Folder && $clipBoardElement->getStorage()->isWithinFolder(
-                        $clipBoardElement,
-                        $this->folderObject,
-                    )
+                            $clipBoardElement,
+                            $this->folderObject
+                        )
                     ) {
                         $addPasteButton = false;
                     }
@@ -324,7 +224,7 @@ class FileListController extends \TYPO3\CMS\Filelist\Controller\FileListControll
                         ])
                         ->setShowLabelText(true)
                         ->setTitle($pastButtonTitle)
-                        ->setIcon($this->iconFactory->getIcon('actions-document-paste-into', self::ICON_SIZE));
+                        ->setIcon($this->iconFactory->getIcon('actions-document-paste-into', IconSize::SMALL));
                     $buttonBar->addButton($pasteButton, ButtonBar::BUTTON_POSITION_LEFT, 10);
                 }
             }
